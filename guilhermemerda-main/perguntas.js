c39722da893e1c96fbd6ef1f59f0afa1ac03cd7c
GatoMexicano <133359376+gatomexicano@users.noreply.github.com>
2024-10-16 14:30:00 -0300

    Add files via upload

---
+++ guilhermemerda-main/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês se diz HI (RAI)'
+)
